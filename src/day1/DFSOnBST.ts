5e2546c7bf350d2dd87fe1bb79bfbaeb5f9c7578
prb01 <p.bergstroem@gmail.com>
2022-11-01 10:55:08 -0300

    passed dfs on binary search tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,10 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+export default function dfs(
+  head: BinaryNode<number> | null,
+  needle: number,
+): boolean {
+  if (!head) return false;
+  if (head.value === needle) return true;
 
+  if (needle > head.value) return dfs(head.right, needle);
+  return dfs(head.left, needle);
 }
