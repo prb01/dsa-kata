2ab716eacdb4dfdc866230faeab4c757c38a36cc
prb01 <p.bergstroem@gmail.com>
2022-11-01 10:13:07 -0300

    passed binary tree breadth-first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,12 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q = [head];
 
+  while (q.length) {
+    const node = q.shift() as BinaryNode<number>;
+    if (node.left) q.push(node.left);
+    if (node.right) q.push(node.right);
+    if (node.value === needle) return true;
+  }
+
+  return false;
 }
