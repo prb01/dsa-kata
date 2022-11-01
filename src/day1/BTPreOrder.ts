5c4827b390832f38cbf1815783c4f8ef2f271fe3
prb01 <p.bergstroem@gmail.com>
2022-11-01 09:41:14 -0300

    passed binary tree pre-order

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,15 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number> | null, path: number[]): number[] {
+  if (!node) {
+    return path;
+  }
 
+  path.push(node.value);
+  walk(node.left, path);
+  walk(node.right, path);
+
+  return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
 }
