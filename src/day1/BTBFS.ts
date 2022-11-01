3a2f4377da39cb134f51102564311c2c04dad548
prb01 <p.bergstroem@gmail.com>
2022-11-01 10:13:48 -0300

    check for value before commiting to add to queue

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -3,9 +3,9 @@ export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
   while (q.length) {
     const node = q.shift() as BinaryNode<number>;
+    if (node.value === needle) return true;
     if (node.left) q.push(node.left);
     if (node.right) q.push(node.right);
-    if (node.value === needle) return true;
   }
 
   return false;
