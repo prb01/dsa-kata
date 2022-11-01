8462c34d0616726ff83d670ef6199ff05d1486f6
prb01 <p.bergstroem@gmail.com>
2022-11-01 09:46:28 -0300

    updated BTPre to define path arr outside

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,15 +1,15 @@
-function walk(node: BinaryNode<number> | null, path: number[]): number[] {
+function walk(node: BinaryNode<number> | null, path: number[]): void {
   if (!node) {
-    return path;
+    return;
   }
 
   path.push(node.value);
   walk(node.left, path);
   walk(node.right, path);
-
-  return path;
 }
 
 export default function pre_order_search(head: BinaryNode<number>): number[] {
-  return walk(head, []);
+  const path: number[] = [];
+  walk(head, path);
+  return path;
 }
