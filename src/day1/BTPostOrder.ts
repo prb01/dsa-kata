1309211a9a910cb925e655dfc48ac349cea09e3a
prb01 <p.bergstroem@gmail.com>
2022-11-01 09:52:49 -0300

    passed binary tree post-order

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,13 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number> | null, path: number[]): void {
+  if (!node) return;
 
+  walk(node.left, path);
+  walk(node.right, path);
+  path.push(node.value);
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+  const path: number[] = [];
+  walk(head, path);
+  return path;
 }
