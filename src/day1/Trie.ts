032f73ea02ba366d6fec3d353af74ec7761e07f5
prb01 <p.bergstroem@gmail.com>
2022-11-02 16:15:38 -0300

    passed trie tree

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,18 +1,102 @@
-export default class Trie {
-    
+type Child<T> = {
+  [key: string]: Node<T>;
+};
 
-    
+type Node<T> = {
+  value: string;
+  parent?: Node<T>;
+  children?: Child<T>;
+  isWord?: boolean;
+};
 
-    constructor() {
+export default class Trie<T> {
+  public length: number;
+  private root?: Node<T>;
+
+  constructor() {
+    this.length = 0;
+    this.root = {
+      value: "0",
+      parent: undefined,
+      children: {},
+      isWord: undefined,
+    };
+  }
+
+  insert(item: string): void {
+    let curr = this.root;
+    for (const char of item) {
+      if (curr && !curr.children) curr.children = {};
+      if (curr && curr.children && !curr.children[char]) {
+        const node = {
+          value: char,
+          parent: curr,
+        };
+        curr.children[char] = node;
+        this.length++;
+      }
+      curr = curr?.children && curr.children[char];
     }
 
-    insert(item: string): void {
+    if (curr) curr.isWord = true;
+  }
 
-}
-    delete(item: string): void {
+  delete(item: string): void {
+    const node = this.getNode(item);
+    if (!node) return;
 
-}
-    find(partial: string): string[] {
+    node.isWord = false;
+    if (!node.children) {
+      this.length--;
+      // recurse up & delete if the char is not part of a word
+      this.deleteUp(node);
+    }
+  }
 
-}
+  find(partial: string): string[] {
+    const node = this.getNode(partial);
+    const words: string[] = [];
+    this.dfs(node, words, partial);
+
+    return words;
+  }
+
+  private deleteUp(node: Node<T> | undefined): void {
+    if (!node?.parent || node?.isWord) return;
+
+    if (node.parent && node.parent.children) {
+      delete node.parent.children[node.value];
+      this.deleteUp(node.parent);
+      node.parent = undefined;
+      node.isWord = undefined;
+    }
+  }
+
+  private dfs(node: Node<T> | undefined, words: string[], word: string): void {
+    if (!node) {
+      return;
+    }
+    if (node.isWord) {
+      words.push(word);
+    }
+
+    for (const char in node.children) {
+      word = word + char;
+      this.dfs(node.children[char], words, word);
+    }
+  }
+
+  private getNode(item: string): Node<T> | undefined {
+    let curr = this.root;
+
+    for (const char of item) {
+      if (curr && curr.children && !curr.children[char]) {
+        return undefined;
+      }
+
+      curr = curr && curr.children && curr.children[char];
+    }
+
+    return curr;
+  }
 }
